7561e35126b86eb02f0d50bb7fb749cc57cbc5d8
JeremyRand <biolizard89@gmail.com>
2019-09-22 05:01:38 +0000

    Add stream_id arg to getaddresshistory command
    The stream_id arg, if set, will force a unique TCP connection to be used
    for each unique stream_id.  This is intended to be used with Tor stream
    isolation.
    
    Conflicts:
    	electrum_nmc/electrum/commands.py

--- electrum_nmc/electrum/commands.py
+++ electrum_nmc/electrum/commands.py
@@ -303,12 +303,12 @@ class Commands:
         return s
 
     @command('n')
-    async def getaddresshistory(self, address):
+    async def getaddresshistory(self, address, stream_id=None):
         """Return the transaction history of any address. Note: This is a
         walletless server query, results are not checked by SPV.
         """
         sh = bitcoin.address_to_scripthash(address)
-        return await self.network.get_history_for_scripthash(sh)
+        return await self.network.get_history_for_scripthash(sh, stream_id=stream_id)
 
     @command('w')
     async def listunspent(self, wallet: Abstract_Wallet = None):
@@ -1390,6 +1390,7 @@ command_options = {
     'fee_level':   (None, "Float between 0.0 and 1.0, representing fee slider position"),
     'from_height': (None, "Only show transactions that confirmed after given block height"),
     'to_height':   (None, "Only show transactions that confirmed before given block height"),
+    'stream_id':   (None, "Stream-isolate the network connection using this stream ID (only used with Tor)"),
     'destination': (None, "Namecoin address, contact or alias"),
     'amount':      (None, "Amount to be sent (in NMC). Type \'!\' to send the maximum available."),
     'allow_existing': (None, "Allow pre-registering a name that already is registered.  Your registration fee will be forfeited until you can register the name after it expires."),
