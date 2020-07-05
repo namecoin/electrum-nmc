190bbce5c24e43bd826004f0fb20f63613e58009
JeremyRand <jeremyrand@airmail.cc>
2020-07-05 05:15:47 +0000

    (WIP) Travis: Add more regtest debug output
    Do not merge this to master branch!

--- electrum/tests/regtest.py
+++ electrum/tests/regtest.py
@@ -37,15 +37,19 @@ class TestLightningAB(TestLightning):
     agents = ['alice', 'bob']
 
     def test_breach(self):
+        print("test_breach")
         self.run_shell(['breach'])
 
     def test_redeem_htlcs(self):
+        print("test_redeem_htlcs")
         self.run_shell(['redeem_htlcs'])
 
     def test_breach_with_unspent_htlc(self):
+        print("test_breach_with_unspent_htlc")
         self.run_shell(['breach_with_unspent_htlc'])
 
     def test_breach_with_spent_htlc(self):
+        print("test_breach_with_spent_htlc")
         self.run_shell(['breach_with_spent_htlc'])
 
 
@@ -53,9 +57,11 @@ class TestLightningABC(TestLightning):
     agents = ['alice', 'bob', 'carol']
 
     def test_forwarding(self):
+        print("test_forwarding")
         self.run_shell(['open'])
         self.run_shell(['alice_pays_carol'])
         self.run_shell(['close'])
 
     def test_watchtower(self):
+        print("test_watchtower")
         self.run_shell(['watchtower'])
